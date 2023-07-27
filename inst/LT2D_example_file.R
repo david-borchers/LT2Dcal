6c033672efe7463d36f55d3325922d02abf2cc4d
david-borchers <dlb@st-andrews.ac.uk>
2023-07-27 15:18:23 +0100

    update with new parameters and some more plotting

--- inst/LT2D_example_file.R
+++ inst/LT2D_example_file.R
@@ -10,23 +10,42 @@ library('LT2D')
 w = 0.15 ; ystart = 0.55
 L = 10 ; A = 2*w*L
 
-# set value of 'true' parameters
-# for simulated data:
-b=c(-7.3287948, 0.9945317)
-logphi <- c(0.02,-4.42)
+# set value of 'true' parameters for simulated data:
+
+# set up and plot detection function
+nx = 100
+ny=100
+xs = seq(0,w,length=nx)
+ys = seq(0,ystart,length=ny)
+h.fun.name = "h1"
+b=c(-3, 0.8)
+h.fun = match.fun(h.fun.name) # make h.fun the function specified via a character variable
+p.vals = p.approx(ys,xs,h.fun,b) # detection function values to plot
+plot(ys,p.vals,type='l',ylim=range(0,p.vals),xlab='Perp. distance, x',ylab=expression(p(x)))
+
+
+# Set up and plot density function 
+pi.fun.name = "pi.chnorm" # specify density function name
+logphi <- c(-0.01,log(0.03)) # density function parameters
+pi.fun = match.fun(pi.fun.name) # make Dfun the function specified via a character variable
+D.pdf = pi.fun(xs,logphi,w) # density pdf values to plot
+plot(xs,D.pdf,type='l',ylim=range(0,D.pdf),xlab='Perp. distance, x',ylab=expression(pi(x)))
 
 # produce simulated data:
 set.seed(3)
-simDat = simXY(50, 'pi.norm',
+simDat = simXY(50, pi.fun.name,
                logphi, 'h1', 
                b, w, 
-               ystart)$locs
+               ystart)
+
+# Look at data:
+plot(simDat)
 
 # create the data.frame:
-all.1s <- rep(1,length(simDat$x))
-obj <- 1:length(simDat$x)
-sim.df <- data.frame(x = simDat$x,
-                     y = simDat$y,
+all.1s <- rep(1,length(simDat$locs$x))
+obj <- 1:length(simDat$locs$x)
+sim.df <- data.frame(x = simDat$locs$x,
+                     y = simDat$locs$y,
                      stratum = all.1s,
                      transect = all.1s,
                      L = L,
