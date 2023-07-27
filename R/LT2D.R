754e96e6aab043cf5a846b250ac015519d9f5d62
david-borchers <dlb@st-andrews.ac.uk>
2023-07-27 10:45:14 +0100

    Added root help documentation for package

---
+++ R/LT2D.R
@@ -0,0 +1,20 @@
+#' LT2D: A package implementing 2-dimensional line transect sampling
+#'
+#' Functions for data generation and fitting of 2-dimensional line transect sampling
+#'
+#' @author
+#' Cal Fagard-Jenkin, David Borchers
+#'
+#' @docType package
+#' @name LT2D
+#' 
+#' @references
+#' Borchers, D.L. and Cox, K.J. 2017 Distance sampling detection functions: 2D or not 2D?. 
+#' Biometrics 73: 593-602..
+#'
+#' @keywords
+#'2D line transect
+"_PACKAGE"
+#' @examples
+#' ## To be added.
+#' NULL
